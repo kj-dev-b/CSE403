2c911c3ea0c27add62d16a194f07d148e185b457
KeeegJordan <keeganbbjordan@gmail.com>
2019-02-12 14:03:13 -0800

    removed tracking for .env and added oauth capabilities for adding slack app

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 require('dotenv').config();
 const express = require('express');
+const request = require('request');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Success! Q Review is active.');
+});
+
+app.post('/test', function(req, res) {
+	res.send('Success! Q Review is in Slack!');
+})
+
+app.get('/oauth', function(req, res) {
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "No OAuth Code"});
+        console.log("No OAuth code");
+    } else {
+        request({
+            url: 'https://slack.com/api/oauth.access',
+            qs: {code: req.query.code, client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET},
+            method: 'GET',
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+            }
+        })
+    }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on port ${process.env.PORT}`);
