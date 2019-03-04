f05fa76d2237437a2e98fa6714296f6ea6cc6954
Theemathas Chirananthavat <theemathas@gmail.com>
2019-03-03 23:21:52 -0800

    Attempt to use probot (WIP, abandoned)

--- src/github.js
+++ src/github.js
@@ -1,3 +1,25 @@
 exports.message = (payload) => {
 	return "Hello from GitHub!";
 }
+
+// From https://github.com/probot/probot/issues/780#issuecomment-446644194
+
+const express = require('@runkit/runkit/express-endpoint/1.0.0')
+const { createProbot } = require('probot') 
+
+const probot = createProbot({
+  id: process.env.APP_ID,
+  port: process.env.PROBOT_PORT,
+  secret: process.env.WEBHOOK_SECRET,
+  cert: process.env.PRIVATE_KEY
+})
+
+const myProbotApp = app => {
+  app.on('...')
+}
+
+probot.load(myProbotApp)
+
+// Register the sub-app against Runkit's special Express app
+const runkitApp = express(exports)
+runkitApp.use(probot.server)
