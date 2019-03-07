0af358ee85d1523ff3e1234cee539bb3ab3a8832
KeegJordan <keeganbbjordan@gmail.com>
2019-03-07 12:52:12 -0800

    added slack bot call to github webhook

--- handlers/github.js
+++ handlers/github.js
@@ -1,3 +1,5 @@
+var bot = require('../src/slackbot.js');
+var db = require('../src/db.js');
 // Handlers for events from github webhooks.
 
 require('dotenv').config();
@@ -14,28 +16,18 @@ webhooks.on('pull_request.opened', async ({id, name, payload}) => {
   pull_request_number = payload.pull_request.number;
   pull_request_link = payload.pull_request.html_url
 
-  username = payload.pull_request.user.login; // Get email?
-  user_email_promise = octokit.users.getByUsername({username}).then((user) => {
-    // console.log('user:', user.data);
-    // TODO Why is the email null???
-    return user.data.email;
-  });
+  username = payload.pull_request.user.login; 
 
   diff_url = payload.pull_request.diff_url;
   diff_promise = axios.get(diff_url).then((response) => response.data);
 
   commits_url = payload.pull_request.commits_url;
   latest_commit_promise = axios.get(commits_url).then((response) => response.data[0].sha);
+  
+  var uid = await db.getUidByGitHubName(username);
+  var diff = await diff_promise;
 
-  // TODO actually do something
-  console.log({
-    repo,
-    pull_request_number,
-    pull_request_link,
-    user_email: await user_email_promise,
-    diff: await diff_promise,
-    latest_commit: await latest_commit_promise
-  });
+  bot.newPR('test', 'ill-quit', uid.uid, diff, pull_request_number)
 });
 
 module.exports = webhooks.middleware
